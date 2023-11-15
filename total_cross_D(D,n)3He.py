9ce3141ceb256ea1aaea52c6068134025d3737af
Ul1anchik <ryg2004@mail.ru>
2023-11-15 18:37:20 +0300

    clean up

--- total_cross_D(D,n)3He.py
+++ total_cross_D(D,n)3He.py
@@ -5,16 +5,20 @@ import numpy as np
 EjL = 0.5000 * 10**3
 EjU = 0.6357 * 10**9
 
+
 def dzeta_i(E):
     return 2 * (math.log(E / EjL) / math.log(EjU / EjL)) - 1
-    
-coef = np.loadtxt("C:/Users/ryg20/OneDrive/Рабочий стол/физика плазмы/bkj_Dn.txt")
+
+
+coef = np.loadtxt("bkj_Dn.txt")
 poly = np.polynomial.Chebyshev(coef)
 
 BOTTOM = EjL
 TOP = EjU
 
-get_range = lambda: range(math.ceil(BOTTOM), math.floor(TOP), (math.floor(TOP) - math.ceil(BOTTOM)) // 100)
+get_range = lambda: range(
+    math.ceil(BOTTOM), math.floor(TOP), (math.floor(TOP) - math.ceil(BOTTOM)) // 1000000
+)
 
 
 x = [i for i in get_range()]
@@ -28,24 +32,17 @@ for n in get_range():
     print(Ej, total_cross)
 
 fig = plt.figure()
-fig.suptitle('Total cross section D(D,n)3He')
+fig.suptitle("Total cross section D(D,n)3He")
 ax = fig.add_subplot(1, 1, 1)
-# ax.plot(x, y, color='black', alpha=1.4, marker='^', markevery=(0, 5), label='D(D,n)3He')
-plt.plot(x, y, color = 'black')
-plt.xlabel('$ \mathcal{E}$ (eV/u)')
-plt.ylabel('$ \sigma$ (mb)')
-plt.ylim([10**(-6), 10**2])
+plt.plot(x, y, color="black")
+plt.xlabel("$ \mathcal{E}$ (eV/u)")
+plt.ylabel("$ \sigma$ (mb)")
+plt.ylim([10 ** (-6), 10**2])
 
+ax.grid(which="minor", alpha=0.5, linestyle="--")
+ax.grid(which="major", alpha=0.5, linestyle="--")
 
-# plt.grid(True, color = "grey", linewidth = "1.0", linestyle = "-.")
-ax.grid(which='minor', alpha=0.5, linestyle = "--" )
-ax.grid(which='major', alpha=0.5, linestyle = "--")
-# plt.loglog(x,y)
-# plt.minorticks_on()
-# plt.grid(True, which="both", axis="both", alpha=0.5, linestyle = "--")
-# plt.show()
-
-plt.xscale('log')
-plt.yscale('log')
+plt.xscale("log")
+plt.yscale("log")
 
 plt.show()
