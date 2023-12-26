631a0f77ba9c86c973051e20130c3be02e92fb05
Ul1anchik <ryg2004@mail.ru>
2023-12-26 14:30:57 +0300

    editing

--- building_curves/Lukianov.py
+++ building_curves/Lukianov.py
@@ -7,13 +7,13 @@ def build_table(const):
     x=[i for i in range(100, 100000, 100 )]
     y = []
     for i in x:
-        values_y = 1000 *(10**(-24))*(const/i)*(exp((-46)/math.sqrt(i)))
+        values_y = 1000*(const/i)*(exp((-46)/math.sqrt(i)))
         y.append(values_y)
     return x,y
 
-def plot_total_cross_section(ax: plt.Axes, const):
+def plot_total_cross_section(ax: plt.Axes):
 
-    x, y = build_table(const)
+    x, y = build_table(300)
     ax.plot(x, y, color="green")
     # ax.set_title(title)
     ax.set_xlabel("$ \mathcal{E}$ (eV/u)")
@@ -22,10 +22,11 @@ def plot_total_cross_section(ax: plt.Axes, const):
     ax.grid(which="major", alpha=0.5, linestyle="--")
     ax.set_xscale("log")
     ax.set_yscale("log")
+   
 
 fig = plt.figure()
 plot_total_cross_section(
-    fig.add_subplot(1, 2, 1), 300
+    fig.add_subplot(1, 2, 1)
 )
 
 plt.show()
