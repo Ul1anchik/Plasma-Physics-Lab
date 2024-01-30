38f5025c5af1f096f771d6927e6bb88077c44115
Ul1anchik <ryg2004@mail.ru>
2024-01-30 14:34:14 +0300

    bosch and lukianov fixed

--- final_rate_coef.py
+++ final_rate_coef.py
@@ -5,6 +5,8 @@ from numpy import exp
 from math import pi
 from scipy.integrate import quad
 
+
+n = 10**(20)
 def build_table_rate_coef(filname):
     EjL = 0.5 * 10**3  # eV
     EjU = 0.6357 * 10**9  # eV
@@ -42,40 +44,37 @@ def build_table_rate_coef(filname):
         integral_res, *_ = quad(lambda y: integrand(y, T), 0, np.inf)
         R = CONST * integral_res
         Ts.append(T)
-        Rs.append(R)
+        Rs.append(integral_res)
         T *= STEP
     return Ts, Rs
 
-n = 10**(20)
 def build_table_Bosch(constants): #H.S.Bosch
 
-    x = [i for i in range(10000, 1000000000, 1000)] #eV
+    x = [i for i in range(1, 100, 1)] #keV
     y = []
-    x_ = []
     with open(constants, 'r') as file:
         Const = dict()
         for line in file:
             key, value = line.split()
             Const[key] = float(value)
-
+    
     for i in x:
         Q = i / (1 - ( (i*(Const['C2']+i*(Const['C4']+i*Const['C6'])))/(1+i*(Const['C3']+i*(Const['C5']+i*Const['C7'])))))
         eps = ((Const['Bg']*Const['Bg'])/(4*Q))**(1/3)
-        sigma = Const['C1'] * Q * math.sqrt(eps/(Const['mc']*Const['mc']*i**(3)))*exp(-3*eps)
+        sigma = Const['C1'] * Q * math.sqrt(eps/(Const['mc']*i**(3)))*exp(-3*eps)
         R = n*n*1/2*sigma
-        y.append(R)
-        x_.append(1000*i) #keV
+        y.append(sigma)
 
     return x,y
 
 
 def build_table_Lukianov():
     y = []
-    x = [i for i in range(1000, 10000000, 1000)] #keV
+    x = [i for i in range(11_000_000, 100_000_000, 1000)] #K
     for i in x:
-        values_y = 7*10**(-10)*(n*n/i**(2/3))*exp((-4.25*10**3)/i**(1/3))
+        values_y = 14*10**(-10)*(1/i**(2/3))*exp((-4.25*10**3)/i**(1/3))
         y.append(values_y)
-    return x,y
+    return [8.617 * 10**(-8) * i for i in x],y
 
 
 def plot_total_cross_section(ax: plt.Axes, filname, constants, title):
@@ -97,15 +96,15 @@ def plot_total_cross_section(ax: plt.Axes, filname, constants, title):
     ax.grid(which="major", alpha=0.5, linestyle="--")
     ax.set_xscale("log")
     ax.set_yscale("log")
-    ax.set_ylim([10**(-38), 10**19])
+    # ax.set_ylim([10**(-38), 10**19])
 
 
 fig = plt.figure()
 plot_total_cross_section(
-    fig.add_subplot(1, 2, 1), "bkj_Dp.txt", "building_rate_coef/constants_T_bosch.txt",  "Maxwellian rate coefficient $D(D,p) T$"
+    fig.add_subplot(1, 2, 1), "bkj_Dp.txt","building_rate_coef/constants_T_bosch2.txt",  "Maxwellian rate coefficient $D(D,p) T$"
 )
 plot_total_cross_section(
-    fig.add_subplot(1, 2, 2), "bkj_Dn.txt", "building_rate_coef/constants_D_bosch.txt", "Maxwellian rate coefficient $D(D,n)^3 He$"
+    fig.add_subplot(1, 2, 2), "bkj_Dn.txt",  "building_rate_coef/constants_D_bosch2.txt", "Maxwellian rate coefficient $D(D,n)^3 He$"
 )
 plt.show()
 
