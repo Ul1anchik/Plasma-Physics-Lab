0368969b3efb4b05fad23926364573c8a282f773
Ul1anchik <ryg2004@mail.ru>
2024-02-15 20:07:44 +0300

    result

--- final_rate_coef.py
+++ final_rate_coef.py
@@ -12,43 +12,56 @@ n = 10 ** (20)
 
 
 def build_table_rate_coef(filname):
-    EjL = 0.5 * 10**3  # eV
-    EjU = 0.6357 * 10**9  # eV
-    V0 = 2.18769126379 * 10**6  # atomic unit of velocity (m/s) // sm
+    EjL = 0.5 * 10**0  # keV
+    EjU = 0.6357 * 10**6  # keV
+    V0 = 2.18769126379 * 10**6 # atomic unit of velocity (m/s) 
+    V0_cm=  2.18769126379 * 10**8
     md_kg = 3.34358320 * 10 ** (-27)
+    md_g = 3.34358320 * 10 ** (-24)
+    md_eV = 1875.612_793 * 10**6
     md = 3.34358320 / 1.661  # deuteron mass (a.e.m)
     mu = md**2 / (2 * md)  # in centre masses (a.e.m)
 
-    A = md_kg * (V0**2) / 2  #  // kg * m(sm)^2 /s^2 => J -> eV
+    A = md_kg * (V0**2) / 4 * 6.24*10**(15) #J -> keV
+
+    # A = md_g * (V0_cm**2) / 2 * 10 ** 11 #Erg -> eV (A = 7841 eV)
+    # A = md_kg * (V0**2) / 2   #  // kg * m^2 /s^2 => J -> eV;  
+    # A =  md_kg * (V0**2) / 2 * 10**(-19) = 0 eV,    A = 10**(-15) J
+    # A = mu * (V0**2) / 2 * 10**(-19) = 10**(-7) eV; A = 10**12 J
 
     def gamma(T):
-        return A / T
+        return  A/(T)
 
     def integrand(y, T):
-        E = y * A
-        assert EjL <= E <= EjU
-        sigma = total_cross_sigma(poly, EjL, EjU, E)
+        E = y * A 
+        # assert EjL <= E <= EjU
+        sigma = total_cross_sigma(poly, EjL, EjU, E) * 10**(-21) #mb -> cm^2
         f = y * exp(-y * gamma(T))
         return sigma * f
 
     coef = np.loadtxt(filname)
     poly = np.polynomial.Chebyshev(coef)
 
-    LOWER = 10**3
-    UPPER = 10**5
-    N = 10
+    
+    LOWER = 1 #keV
+    UPPER = 60
+
+    # LOWER = 10**3 #eV
+    # UPPER = 10**5
+    N = 20
     STEP = pow(UPPER / LOWER, 1 / N)
 
     T = LOWER
 
     Ts = []
     Rs = []
+    
     while T < UPPER:
-        CONST = 2 / math.sqrt(pi) * V0 * gamma(T) ** (3 / 2)
-        integral_res, *_ = quad(lambda y: integrand(y, T), 10 ** (17), 10 ** (22))
-        R = CONST * integral_res
-        Ts.append(T / 10**3)
-        Rs.append(R / 10**34)
+        CONST = 2 / math.sqrt(pi) * V0_cm * gamma(T) ** (3 / 2)
+        integral_res, *_ = quad(lambda y: integrand(y, T), 10 ** (-2), 10 ** (1))
+        R = CONST * integral_res 
+        Ts.append(T ) # keV
+        Rs.append(R / 10**6)
         T *= STEP
     return Ts, Rs
 
@@ -87,7 +100,7 @@ def build_table_Lukianov():
     x = [i for i in range(11_000_000, 100_000_000, 1000)]  # K
     for i in x:
         values_y = (
-            14
+            7
             * 10 ** (-10)
             * (1 / i ** (2 / 3))
             * exp((-4.25 * 10**3) / i ** (1 / 3))
@@ -113,8 +126,8 @@ def plot_total_cross_section(ax: plt.Axes, filname, constants, title):
     ax.plot(x, y, color="green", label="using S.U.Lukianov formula")
 
     ax.set_title(title)
-    ax.set_xlabel("T")
-    ax.set_ylabel("$\\langle\sigma V\\rangle$")
+    ax.set_xlabel("$T(keV)$")
+    ax.set_ylabel("$\\langle\sigma V\\rangle (cm^3/s)$")
     ax.grid(which="minor", alpha=0.5, linestyle="--")
     ax.grid(which="major", alpha=0.5, linestyle="--")
     ax.set_xscale("log")
